fe449701c6024867140df496a8b1eeaf7fea3781
JMag <jmaghamez@gmail.com>
2025-04-23 21:34:54 -0400

    Implemented, "Save Your Work" warning

--- Scripts/editor.js
+++ Scripts/editor.js
@@ -1,14 +1,31 @@
 const fileMenu = document.getElementById("fileMenu");
 const editMenu = document.getElementById("editMenu");
+const helpMenu = document.getElementById("helpMenu");
+
+const dialogFocusBackground = document.getElementById("dialogFocusBackground");
 
 function hideAndShow(hiddenContainer, shownContainer) {
   document.getElementById(hiddenContainer).hidden = true;
   document.getElementById(shownContainer).hidden = false;
 }
 
+function toggleDialog(isShown, currentDialog) {
+  switch (isShown) {
+    case true:
+      dialogFocusBackground.hidden = false;
+      document.getElementById(currentDialog).hidden = false;
+      break;
+    case false:
+      dialogFocusBackground.hidden = true;
+      document.getElementById(currentDialog).hidden = true;
+      break;
+  }
+}
+
 function hideAllMenuDropdowns() {
   fileMenu.hidden = true;
   editMenu.hidden = true;
+  helpMenu.hidden = true;
 }
 
 function toggleMenuDropdown(currentDropdown) {
--- Scripts/index.js
+++ Scripts/index.js
@@ -4,9 +4,9 @@ const toolbarOptions = [
   ['link', 'image', 'video', 'formula'],
 
   [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+  [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
   [{ 'direction': 'rtl' }],                         // text direction
 
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
